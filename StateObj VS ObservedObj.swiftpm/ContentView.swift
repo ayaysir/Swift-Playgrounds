ea7d8bf72561d278bfdce314b48a992b6d415b3a
윤범태 <yoonbumtae@gmail.com>
2024-01-29 00:10:52 +0900

    Added: StateObject vs ObservedObject

--- StateObj VS ObservedObj.swiftpm/ContentView.swift
+++ StateObj VS ObservedObj.swiftpm/ContentView.swift
@@ -1,12 +1,60 @@
 import SwiftUI
 
-struct ContentView: View {
+class RandomGenerator: ObservableObject {
+    @Published private(set) var text = UUID().uuidString.split(separator: "-")[0]
+    @Published var word = "👀"
+    
+    init() {
+        text += " \(Date.now.timeIntervalSince1970)"
+    }
+}
+
+struct RandomView1: View {
+    @ObservedObject var rg: RandomGenerator = .init()
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        Text(rg.text)
+        Button("아무말") {
+            rg.word = ["🐼", "🐟", "🦀", "🐴"].randomElement() ?? ""
+        }
+        Text(rg.word)
+    }
+}
+
+struct RandomView2: View {
+    @StateObject var rg: RandomGenerator = .init()
+    
+    var body: some View {
+        Text(rg.text)
+        Button("아무말") {
+            rg.word = ["🐼", "🐟", "🦀", "🐴"].randomElement() ?? ""
+        }
+        Text(rg.word)
+    }
+}
+
+struct ContentView: View {
+    @State private var isOn = false
+    
+    var body: some View {
+        NavigationStack {
+            VStack {
+                Button(isOn ? "켬" : "꺼짐") {
+                    isOn.toggle()
+                }
+                Divider()
+                
+                Text("@ObservedObject")
+                    .font(.custom("Courier New", size: 20).bold())
+                RandomView1()
+                Divider()
+                
+                Text("@StateObject")
+                    .font(.custom("Courier New", size: 20).bold())
+                RandomView2()
+                Divider()
+            }
         }
     }
+
 }
